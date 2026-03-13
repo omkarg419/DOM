d9d1c7420bc55b1c904238b863900ca348a63d7e
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2026-03-13 20:41:13 +0530

    Refactor HTML and CSS for improved readability and consistency

--- JS_DOM_projects/dark-mode-toggle/main.js
+++ JS_DOM_projects/dark-mode-toggle/main.js
@@ -3,12 +3,11 @@ const header = document.getElementById("header");
 toggleBtn.addEventListener("click", () => {
 	const mode = document.body.classList.toggle("dark-mode");
 
-	
 	if (mode) {
 		toggleBtn.textContent = "Toggle Light Mode";
-        header.textContent = "Dark Mode Toggled";
+		header.textContent = "Dark Mode Toggled";
 	} else {
 		toggleBtn.textContent = "Toggle Dark Mode";
-        header.textContent = "Light Mode Toggled";
+		header.textContent = "Light Mode Toggled";
 	}
 });
